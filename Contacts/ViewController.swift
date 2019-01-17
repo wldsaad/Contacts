a2bb2303033e8c945b56ae22763f0fae8eee9b61
Waleed Saad <waleedsaad@Waleeds-MacBook-Air.local>
2019-01-17 23:48:03 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Waleed Saad on 1/17/19.
+//  Copyright © 2019 Waleed Saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
